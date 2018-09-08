806810a698662ffbabc5a173a2f0bb1a53a6e0a4
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2018-09-08 11:00:15 -0400

    beakers are hilarous

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Draggable : MonoBehaviour {
-    private Vector3 offset;
-
-    void OnMouseDown()
+namespace Assets.Scripts
+{
+    public class Draggable : MonoBehaviour
     {
+        private Vector3 offset;
+        private Rigidbody2D rb;
 
-        offset = gameObject.transform.position -
-            Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
-    }
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
 
-    void OnMouseDrag()
-    {
-        Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
-        transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+        void OnMouseDown()
+        {
+
+            offset = gameObject.transform.position -
+                Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+        }
+
+        void OnMouseDrag()
+        {
+            Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+            transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+            rb.velocity = Vector2.zero;
+        }
     }
 }
